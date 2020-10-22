1fb0909821a8ab29c7d61accf551e7197db3fa8a
“brunobatista” <brunobatista101@gmail.com>
2020-10-22 13:31:24 -0300

    add trailing comma

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.SCHEMA,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
-    logging: false
+    logging: false,
   },
   test: {
     username: process.env.MYSQL_USER,
@@ -15,7 +15,7 @@ module.exports = {
     database: process.env.SCHEMA,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
-    logging: false
+    logging: false,
   },
   production: {
     username: process.env.MYSQL_USER,
@@ -23,6 +23,6 @@ module.exports = {
     database: process.env.SCHEMA,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
-    logging: false
+    logging: false,
   },
 };
